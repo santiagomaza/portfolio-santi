aa8016ff70d1e9b18009ae8b1065213fe1d08105
Santiago Maza <santiagommaza@gmail.com>
2024-05-20 12:26:02 -0300

    Se agregó un apartado de proyectos en main

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,6 +5,10 @@ export const Main = () => {
     <>
       <div className="container d-flex justify-content-center text-center flex-wrap">
         <p>¡Hola a todos! Mi nombre es <strong>Santiago Maza</strong>, estudiante de la carrera Tecnicatura Universitaria en Programación de la Universidad Tecnológica Nacional de Tucumán.</p>
+        <p className='segundo-texto'>Sin tener demasiada experiencia en este campo, estoy constantemente buscando información sobre nuevas tecnologías o de las que ya manejo pero que me gustaría perfeccionar.</p>
+      </div>
+      <div className="container-fluid">
+        <span className='fst-italic fst-underlined text-uppercase text-decoration-underline pruebaTexto'>Proyectos</span>
       </div>
     </>
   )
