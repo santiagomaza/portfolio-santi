1c7beb90e4029c56738cffe1677ec3666a4f20a3
Santiago Maza <santiagommaza@gmail.com>
2024-05-22 12:28:00 -0300

    Se agregaron iconos a la seccion de proyectos y se creó una sección de tecnologias

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -11,11 +11,16 @@ export const Main = ({ nombreAlumno, carrera, univ }) => {
         <p>¡Hola a todos! Mi nombre es <strong>{nombreAlumno}</strong>, estudiante de la carrera {carrera} de la {univ}</p>
         <p className='segundo-texto'>Sin tener demasiada experiencia en este campo, estoy constantemente buscando información sobre nuevas tecnologías o de las que ya manejo pero que me gustaría perfeccionar.</p>
       </div>
+      <div className='border-bottom border-dark mb-3 mt-3 w-50 m-auto'></div>
       <div className="container-fluid">
         <span className='fst-italic fst-underlined text-uppercase text-decoration-underline subtituloProyecto mb-3'>Proyectos</span>
         <Proyectos {...ecomrcProy}/>
         <Proyectos {...kiosquitoelbarbaProy}/>
         <Proyectos {...theFilmZoneProy}/>
+      </div>
+      <div className='border-bottom border-dark mb-3 mt-3 w-50 m-auto'></div>
+      <div className="container-fluid">
+        <span className='fst-italic fst-underlined text-uppercase text-decoration-underline subtituloProyecto2 mb-3'>Tecnologias</span>
       </div>
     </>
   )
--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -1,3 +1,7 @@
+import { FaGithub } from "react-icons/fa";
+import { CiGlobe } from "react-icons/ci";
+
+
 export const Proyectos = (props) => {
   const { nombreProyecto, descripcionProyecto, tecnologiasUsadas, urlProyecto, urlGithub, urlImagen, textoAlt } = props
 
@@ -5,12 +9,12 @@ export const Proyectos = (props) => {
     <div className="row mb-3">
       <div className='col-lg-5'>
         <h5 className='d-block fw-bold'>{nombreProyecto}</h5>
-        <p className='fs-6 lh-sm'>{descripcionProyecto}</p>
+        <p className='fs-6 lh-sm h-25'>{descripcionProyecto}</p>
         <span className='fw-bold'>Tecnologias Usadas:</span>
         <p className='fst-italic font-monospace letter-spacing-1'>{tecnologiasUsadas}</p>
-        <div className="d-flex justify-content-end align-items-end">
-          <a href={urlProyecto} target="_blank" rel="noreferrer" className='btn btn-outline-dark me-2'>Ver Proyecto</a>
-          <a href={urlGithub} target="_blank" rel="noreferrer" className='btn btn-outline-dark'>Ir a Github</a>
+        <div className="d-flex justify-content-end">
+          <a href={urlProyecto} target="_blank" rel="noreferrer" className='btn btn-outline-dark me-2'><CiGlobe className="me-2 fs-5 bg-transparent"/>Ver Proyecto</a>
+          <a href={urlGithub} target="_blank" rel="noreferrer" className='btn btn-outline-dark'><FaGithub className="me-2 fs-5 bg-transparent"/>Ir a Github</a>
         </div>
       </div>
       <img src={urlImagen} alt={textoAlt} style={{width: "750px", height: "350px"}}/>
